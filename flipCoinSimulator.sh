2d5cb68494aa450c12cd4c9b797fed958dc9aa43
Preranashinde <preranapshinde3009@gmail.com>
2020-03-24 18:09:03 -0300

    Store the doublet combination as well as the percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
 declare -A SingletCoinDictionary
-headPercentage=0
+declare -A DoubletCoinDictionary
 
 #Store the singlet combination result
-read -p "Enter how many times you want to flip a coin for singlet combination:" FlipCoin
+read -p "Enter how many times you want to flip a coin for singlet combination:" FlipCoin1
 
-for((counter=1; counter<=FlipCoin; counter++))
+for((counter=1; counter<=FlipCoin1; counter++))
 
 do
 	result=$((RANDOM%2))
@@ -19,14 +19,57 @@ do
 done
 
 #To print dictionary output
-for output in "${!SingletCoinDictionary[@]}"
+for output in  "${!SingletCoinDictionary[@]}"
 do
 	echo ""${outut}": "${SingletCoinDictionary[$output]}""
 done
 
 #To calculate percentage of singlet combination
-headPercentage=`echo " scale = 4 ; (${SingletCoinDictionary[Head]}*100)/$FlipCoin " | bc -l`
+headPercentage=`echo " scale = 4 ; (${SingletCoinDictionary[Head]}*100)/$FlipCoin1 " | bc -l`
 tailPercentage=`echo " scale = 4 ; 100-$headPercentage" | bc -l`
 echo "Percentage of heads :" $headPercentage
 echo "Percentage of tails :" $tailPercentage
+
+#Store the doublet combination result
+read -p "Enter how many times you want to flip a coin for doublet combination:" FlipCoin2
+
+for((counter=1; counter<=FlipCoin2; counter++))
+
+do
+	result=$((RANDOM%4))
+	if [ $result -eq 0 ]
+   then
+      DoubletCoinDictionary[HeadHead]=$((${DoubletCoinDictionary[HeadHead]}+1))
+   elif [ $result -eq 1 ]
+	then
+      DoubletCoinDictionary[HeadTail]=$((${DoubletCoinDictionary[HeadTail]}+1))
+	elif [ $result -eq 2 ]
+	then
+      DoubletCoinDictionary[TailHead]=$((${DoubletCoinDictionary[TailHead]}+1))
+	else
+      DoubletCoinDictionary[TailTail]=$((${DoubletCoinDictionary[TailTail]}+1))
+	fi
+done
+
+#To print doublet dictionary output
+for output in "${!DoubletCoinDictionary[@]}"
+do
+   echo ""${outut}": "${DoubletCoinDictionary[$output]}""
+done
+
+#To calculate percentage of doublet combination
+PercentageOfHeadHead=`echo " scale = 4 ; (${DoubletCoinDictionary[HeadHead]}*100)/$FlipCoin2" | bc -l`
+echo "PercentageOfHeadHead::" $PercentageOfHeadHead
+PercentageOfHeadTail=`echo " scale = 4 ; (${DoubletCoinDictionary[HeadTail]}*100)/$FlipCoin2" | bc -l`
+echo "PercentageOfHeadTail::" $PercentageOfHeadTail
+PercentageOfTailHead=`echo " scale = 4 ; (${DoubletCoinDictionary[TailHead]}*100)/$FlipCoin2" | bc -l`
+echo "PercentageOfTailHead::" $PercentageOfTailHead
+PercentageOfTailTail=`echo " scale = 4 ; (${DoubletCoinDictionary[TailTail]}*100)/$FlipCoin2" | bc -l`
+echo "PercentageOfTailTail::" $PercentageOfTailTail
+
+
+
+
+
+
 
