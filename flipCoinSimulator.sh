e095beaccf12dbc0d0dcbb2bcb6bbde9da8b1293
Preranashinde <preranapshinde3009@gmail.com>
2020-03-24 12:10:45 -0300

    Store singlet combination in a dictionary and determine percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,32 @@
 #!/bin/bash -x
 
-FlipCoin=$(($(($RANDOM%10))%2))
+declare -A SingletCoinDictionary
+headPercentage=0
 
-if [ $FlipCoin -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+#Store the singlet combination result
+read -p "Enter how many times you want to flip a coin for singlet combination:" FlipCoin
+
+for((counter=1; counter<=FlipCoin; counter++))
+
+do
+	result=$((RANDOM%2))
+	if [ $result -eq 0 ]
+	then
+		SingletCoinDictionary[Head]=$((${SingletCoinDictionary[Head]}+1)) 
+	else
+		SingletCoinDictionary[Tail]=$((${SingletCoinDictionary[Tail]}+1)) 
+	fi
+done
+
+#To print dictionary output
+for output in "${!SingletCoinDictionary[@]}"
+do
+	echo ""${outut}": "${SingletCoinDictionary[$output]}""
+done
+
+#To calculate percentage of singlet combination
+headPercentage=`echo " scale = 4 ; (${SingletCoinDictionary[Head]}*100)/$FlipCoin " | bc -l`
+tailPercentage=`echo " scale = 4 ; 100-$headPercentage" | bc -l`
+echo "Percentage of heads :" $headPercentage
+echo "Percentage of tails :" $tailPercentage
+
